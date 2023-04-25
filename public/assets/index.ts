1db834a193f53396bb991be2ecbf4864d0cac1bd
Erik <rikr9879@gmail.com>
2023-04-25 11:26:19 +0700

    ubah background color ketika hover

--- public/assets/index.ts
+++
@@ -1,8 +0,0 @@
-import Delete from "./delete.svg";
-import Arrow from "./arrow.svg";
-import Plus from "./plus.svg";
-import SettingIcon from "./setting.svg";
-import ThreeDots from "./three-dots.svg";
-import Hamburger from "./hamburger-icon.svg";
-
-export { Delete, Arrow, Plus, SettingIcon, ThreeDots, Hamburger };
