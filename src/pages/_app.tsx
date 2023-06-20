78af5dc42c6f34132d98926f20cea8f5c26c0fbe
Erik <rikr9879@gmail.com>
2023-06-20 17:21:53 +0700

    remove router event when unmount

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps, AppContext } from "next/app";
 import App from "next/app";
-import Router from "next/router";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 import PocketProvider from "@/context/PocketProvider";
 
@@ -11,17 +11,41 @@ import "@/styles/globals.css";
 export default function MyApp({
   Component,
   pageProps,
-  API_URL
+  API_URL,
 }: AppProps & {
   Component: { getLayout?: (page: React.ReactElement) => JSX.Element };
-  API_URL: string
+  API_URL: string;
 }) {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const getLayout = Component.getLayout ?? ((page) => page);
+  const router = useRouter();
 
-  Router.events.on("routeChangeStart", () => setIsLoading(() => true));
-  Router.events.on("routeChangeComplete", () => setIsLoading(() => false));
-  Router.events.on("routeChangeError", () => setIsLoading(() => false));
+  useEffect(() => {
+    const handleRouteChangeStart = () => setIsLoading(() => true);
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+    };
+  }, [router]);
+
+  useEffect(() => {
+    const handleRouteChangeComplete = () => setIsLoading(() => false);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeComplete);
+    };
+  }, [router]);
+  
+  useEffect(() => {
+    const handleRouteChangeError = () => setIsLoading(() => false);
+    router.events.on("routeChangeError", handleRouteChangeError);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeError);
+    };
+  }, [router]);
 
   return (
     <PocketProvider apiUrl={API_URL}>
@@ -39,11 +63,11 @@ export default function MyApp({
 }
 
 MyApp.getInitialProps = async (ctx: AppContext) => {
-  const appProps = await App.getInitialProps(ctx)
-  const API_URL = process.env.API_URL
+  const appProps = await App.getInitialProps(ctx);
+  const API_URL = process.env.API_URL;
 
   return {
     ...appProps,
-    API_URL
-  }
-}
+    API_URL,
+  };
+};
